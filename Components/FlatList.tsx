9817e7cd5333c96a7cca11941b552710e318d0df
Mallappa <mallappa.k@digilearn.online>
2024-08-26 17:05:38 +0530

    Testing

--- Components/FlatList.tsx
+++ Components/FlatList.tsx
@@ -7,6 +7,8 @@ const App = () => {
     { id: '2', title: 'Item 2' },
     { id: '3', title: 'Item 3' },
     { id: '4', title: 'Item 4' },
+    { id: '5', title: 'Item 5' },
+    
   ];
 
   const renderItem = ({ item }: { item: { id: string; title: string } }) => (
